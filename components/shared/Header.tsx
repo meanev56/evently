aa7c143eeafc79d0711dfe0dbefc7a16d2ca9850
meanev56 <emekanjoku86@gmail.com>
2024-01-19 04:30:59 +0100

    constants

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,8 +1,10 @@
-import { SignedOut } from '@clerk/nextjs'
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from '../ui/button'
+import NavItems from './NavItems'
+import MobileNav from './MobileNav'
 
 const Header = () => {
   return (
@@ -20,7 +22,17 @@ const Header = () => {
                 />
             </Link>
 
+            <SignedIn>
+              <nav className='md:flex-between hidden w-full max-w-xs'>
+                <NavItems />
+              </nav>
+            </SignedIn>
+
             <div className='flex w-32 justify-end gap-3'>
+              <SignedIn>
+                <UserButton afterSignOutUrl='/' />
+                <MobileNav />
+              </SignedIn>
               <SignedOut>
                 <Button asChild className='rounded-full' size='lg'>
                   <Link href='signed-in'>
@@ -28,11 +40,11 @@ const Header = () => {
                   </Link>
                 </Button>
               </SignedOut>
-                Header
-            </div>
-            Heade
+                
+            Sign</div>
+            
         </div>
-      Head
+      
     </header>
   )
 }
